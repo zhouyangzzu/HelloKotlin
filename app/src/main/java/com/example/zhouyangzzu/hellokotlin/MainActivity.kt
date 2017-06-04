d50263fa8e3eeadbc00ee401671e9043b1900dbf
zhouyangzzu <zhouyangzzu@gmail.com>
2017-06-04 11:19:46 +0800

    add viewpager

--- app/src/main/java/com/example/zhouyangzzu/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/zhouyangzzu/hellokotlin/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.zhouyangzzu.hellokotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.PagerAdapter
+import com.tmall.ultraviewpager.UltraViewPager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUltraViewPager()
+    }
+
+    fun initUltraViewPager(){
+        var vp: UltraViewPager = findViewById(R.id.ultra_viewpager) as UltraViewPager
+        vp.setScrollMode(UltraViewPager.ScrollMode.HORIZONTAL)
+
+        var adapter: PagerAdapter = MyPageAdapter(this)
+        vp.adapter = adapter
+
     }
 }
---
+++ app/src/main/java/com/example/zhouyangzzu/hellokotlin/MyPageAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.zhouyangzzu.hellokotlin
+
+import android.content.Context
+import android.support.v4.view.PagerAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+
+/**
+ * Created by zhouyangzzu on 2017/6/4.
+ */
+
+class MyPageAdapter (var mContext:Context): PagerAdapter(){
+
+
+    override fun isViewFromObject(p0: View?, p1: Any?): Boolean {
+        return p0 == p1
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun instantiateItem(container: ViewGroup?, position: Int): Any {
+
+        var layoutId = 0
+        when(position){
+            0 -> layoutId = R.layout.layout_mine_stock
+            1 -> layoutId = R.layout.layout_mine_news
+        }
+
+        var layout : LinearLayout = LayoutInflater.from(mContext).inflate(layoutId, null) as LinearLayout
+        if (container != null)
+            container.addView(layout)
+
+        return layout
+    }
+
+    override fun destroyItem(container: ViewGroup?, position: Int, `object`: Any?) {
+        if (container != null)
+            container.removeView(`object` as View)
+    }
+}
