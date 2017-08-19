b0e7f63ae32e830e3845e03b5bea61b0130a6234
zhouyangzzu <zhouyangzzu@gmail.com>
2017-08-19 19:57:15 +0800

    添加语音提示

--- app/src/main/java/com/example/zhouyangzzu/hellokotlin/service/AlarmReceiver.java
+++ app/src/main/java/com/example/zhouyangzzu/hellokotlin/service/AlarmReceiver.java
@@ -3,7 +3,10 @@ package com.example.zhouyangzzu.hellokotlin.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 /**
  * Created by zhouyangzzu on 2017/8/19.
@@ -11,8 +14,22 @@ import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
+    static TextToSpeech textToSpeech = null;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "Toast", Toast.LENGTH_SHORT).show();
+
+        if (null == textToSpeech){
+            textToSpeech = new TextToSpeech(context.getApplicationContext(), new TextToSpeech.OnInitListener() {
+                @Override
+                public void onInit(int status) {
+                    textToSpeech.setLanguage(Locale.ENGLISH);
+                }
+            });
+        }else{
+            textToSpeech.speak("EOS rise 5%", TextToSpeech.QUEUE_FLUSH, null);
+        }
     }
+
 }
